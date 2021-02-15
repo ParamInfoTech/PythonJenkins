94bfcbbe7da891573228e02e61b11d70c2c44aca
katkarvijayd <katkarvijayd@users.noreply.github.com>
2021-02-15 14:33:26 +0530

    Create mycalc.py

---
+++ mycalc.py
@@ -0,0 +1,12 @@
+print('PyCharm')
+def add(x,y):
+    return x + y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot Divide by 0")
+    else:
+        return x/y
+
+def subtract(x,y):
+    return x-y
